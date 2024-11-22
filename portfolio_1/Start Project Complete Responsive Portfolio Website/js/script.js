cbcf80a82a5a14e6d6bcca61feab9d51d8458622
hamza-soilihi <soilihihamza2@gmail.com>
2024-11-22 13:27:18 +0100

    ajout du portfolio

---
+++ portfolio_1/Start Project Complete Responsive Portfolio Website/js/script.js
@@ -0,0 +1,32 @@
+/*========== menu icon navbar ==========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+/*========== scroll sections active link ==========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+
+
+    /*========== sticky navbar ==========*/
+window.onscroll = () => {
+let header = document.querySelector('.header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+// problem sur mon header qd je vais sur about
+    /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
+
+
+
+
+/*========== swiper ==========*/
+
+
+/*========== dark light mode ==========*/
+
+
+/*========== scroll reveal ==========*/
